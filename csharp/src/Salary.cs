d6ad85702b69708fb0ed07cdc0fc33207e236d4b
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-09-09 08:06:54 +0500

    inline classes

--- csharp/src/Salary.cs
+++ csharp/src/Salary.cs
@@ -14,145 +14,77 @@ public class Salary
 
     public decimal CalculateTaxFreeAllowance()
     {
-        return GetPersonalAllowance().Get();
-    }
-
-    private IPersonalAllowance GetPersonalAllowance()
-    {
-        IPersonalAllowance result;
-        if (Amount > 100000)
+        const decimal PersonalAllowance = 11000;
+        if (Amount > 100000 && Amount <= 122000)
         {
-            result = new ReducedPersonalAllowance(new DefaultPersonalAllowance(), this);
+            return PersonalAllowance - (Amount - 100000) / 2;
+        }
+        else if (Amount < 100000)
+        {
+            return PersonalAllowance;
         }
         else
         {
-            result = new DefaultPersonalAllowance();
+            return 0;
+        }
+    }
+
+    public decimal CalculateTaxPayable()
+    {
+        var amount = CalculateTaxableIncome();
+        var result = 0M;
+        if (Amount > 11000)
+        {
+            var taxable = amount;
+            if (taxable > 32000)
+            {
+                taxable = 32000;
+            }
+            result += taxable * 0.2M;
+            amount -= taxable;
+        }
+        if (Amount > 43000)
+        {
+            var taxable = amount;
+            if (taxable > 118000)
+            {
+                taxable = 118000;
+            }
+            result += taxable * 0.4M;
+            amount -= taxable;
+        }
+        if (Amount > 150000)
+        {
+            result += amount * 0.45M;
         }
 
         return result;
     }
 
-    public decimal CalculateTaxPayable()
-    {
-        var contributions = new List<IContributions>();
-        if (Amount > 11000)
-        {
-            contributions.Add(new BasicRate());
-        }
-        if (Amount > 43000)
-        {
-            contributions.Add(new HigherRate());
-        }
-        if (Amount > 150000)
-        {
-            contributions.Add(new AdditionalRate());
-        }
-
-        return contributions.Sum(x => x.Calculate(this));
-    }
-
     public decimal CalculateNiContributions()
     {
-        var contributions = new List<IContributions>();
+        var amount = GetContributableIncome();
+        var result = 0M;
         if (Amount > 8060)
         {
-            contributions.Add(new BasicContributions());
+            var taxable = amount;
+            if (taxable > 34940)
+            {
+                taxable = 34940;
+            }
+            result += taxable * 0.12M;
+            amount -= taxable;
         }
         if (Amount > 43000)
         {
-            contributions.Add(new HigherContributions());
+            result += amount * 0.02M;
         }
 
-        return contributions.Sum(x => x.Calculate(this));
-    }
-}
-
-public interface IPersonalAllowance
-{
-    decimal Get();
-}
-
-public class DefaultPersonalAllowance : IPersonalAllowance
-{
-    public decimal Get() => 11000;
-}
-
-public class ReducedPersonalAllowance : IPersonalAllowance
-{
-    private readonly IPersonalAllowance defaultAllowance;
-    private readonly Salary salary;
-
-    public ReducedPersonalAllowance(IPersonalAllowance defaultAllowance, Salary salary)
-    {
-        this.defaultAllowance = defaultAllowance;
-        this.salary = salary;
+        return result;
     }
 
-    public decimal Get()
+    private decimal GetContributableIncome()
     {
-        var result = defaultAllowance.Get() - (salary.Amount - 100000) / 2;
-        return Math.Max(0, result);
-    }
-}
-
-public interface IContributions
-{
-    decimal Calculate(Salary salary);
-}
-
-public class BasicContributions : IContributions
-{
-    private const decimal Rate = 0.12M;
-    private const decimal Max = 34940;
-    private const decimal ContributionFreeAllowance = 8060;
-
-    public decimal Calculate(Salary salary)
-    {
-        return Math.Min(salary.Amount - ContributionFreeAllowance, Max) * Rate;
-    }
-}
-
-public class HigherContributions : IContributions
-{
-    private const decimal Rate = 0.02M;
-    private const decimal Max = 43000;
-
-    public decimal Calculate(Salary salary)
-    {
-        return (salary.Amount - Max) * Rate;
-    }
-}
-
-public class BasicRate : IContributions
-{
-    private const decimal Rate = 0.2M;
-    private const decimal Max = 32000;
-
-    public decimal Calculate(Salary salary)
-    {
-        return Math.Min(salary.CalculateTaxableIncome(), Max) * Rate;
-    }
-}
-
-public class HigherRate : IContributions
-{
-    private const decimal Rate = 0.4M;
-    private const decimal Max = 118000;
-    private const int AlreadyTaxed = 32000;
-
-    public decimal Calculate(Salary salary)
-    {
-        return Math.Min(salary.CalculateTaxableIncome() - AlreadyTaxed, Max) * Rate;
-    }
-}
-
-public class AdditionalRate : IContributions
-{
-    private const decimal Rate = 0.45M;
-    private const int AlreadyTaxed = 150000;
-
-    public decimal Calculate(Salary salary)
-    {
-        return (salary.CalculateTaxableIncome() - AlreadyTaxed) * Rate;
+        return Math.Max(0, Amount - 8060);
     }
 }
